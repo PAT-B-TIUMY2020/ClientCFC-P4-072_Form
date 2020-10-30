f89d5c204b804e7303f5aaec552cbc6452196153
Nuraini <nurroplet45@gmail.com>
2020-10-30 18:42:21 +0700

    Add project files.

---
+++ ClientCFC-P4-20180140072_Form/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientCFC_P4_20180140072_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int hasil;
+            hasil = 1 + 2;
+            label2.Text = hasil.ToString();
+            hasil = 3 - 2;
+            label4.Text = hasil.ToString();
+            hasil = 2 * 2;
+            label6.Text = hasil.ToString();
+            hasil = 2 / 2;
+            label8.Text = hasil.ToString();
+
+        }
+    }
+}
